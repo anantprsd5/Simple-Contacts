2aeec80f7df8af9882985f50b64607aa85dab873
Anant Prasad <anantprsd5@gmail.com>
2019-04-29 21:47:41 +0530

    Final touch

--- app/src/main/java/com/pratilipi/contacts/Activity/MainActivity.java
+++ app/src/main/java/com/pratilipi/contacts/Activity/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED) {
             requestContactPermission();
+
         } else {
             mainActivityPresenter.getContacts(0, isChanged);
         }
@@ -76,25 +77,19 @@ public class MainActivity extends AppCompatActivity implements MainView {
                 != PackageManager.PERMISSION_GRANTED) {
 
             // Permission is not granted
-            // Should we show an explanation?
+
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_CONTACTS)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
+
             } else {
                 // No explanation needed; request the permission
                 Toast.makeText(this, "asd",Toast.LENGTH_SHORT).show();
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_CONTACTS},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         } else {
-            // Permission has already been granted
+
         }
     }
 
@@ -145,14 +140,11 @@ public class MainActivity extends AppCompatActivity implements MainView {
                     mainActivityPresenter.getContacts(0, isChanged);
 
                 } else {
-                    Toast.makeText(this, "This app needs your read contacts permission",
+                    Toast.makeText(this, getString(R.string.permission_required),
                             Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
         }
     }
 
